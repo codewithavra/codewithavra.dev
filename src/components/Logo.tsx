ee449dc219a9298c7b8ad0aa0a9b7ce3c3511bd3
codewithavra <244584119+codewithavra@users.noreply.github.com>
2026-04-30 16:54:30 +0530

    Replace favicon with new logo files and update Navbar to include the Logo component. Remove unused SVG files for icons and favicon.

---
+++ src/components/Logo.tsx
@@ -0,0 +1,25 @@
+/**
+ * @license Apache-2.0
+ * @copyright codewithavra
+ */
+
+export const Logo = ({ className }: { className?: string }) => (
+    <svg
+      className={`text-foreground h-auto w-5 ${className ?? ''}`}
+      viewBox="0 0 1416 466"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="111" y="2" width="244" height="111" fill="currentColor"/>
+      <path d="M1063 0H1307V111H1063V0Z" fill="currentColor"/>
+      <path d="M1063 234H1307V345H1063V234Z" fill="currentColor"/>
+      <rect x="732.392" y="198" width="244" height="111" transform="rotate(67.2868 732.392 198)" fill="currentColor"/>
+      <rect x="697.595" y="240.854" width="244" height="111" transform="rotate(112.71 697.595 240.854)" fill="currentColor"/>
+      <rect x="488" y="466" width="464" height="111" transform="rotate(-90 488 466)" fill="currentColor"/>
+      <rect x="732" y="466" width="464" height="111" transform="rotate(-90 732 466)" fill="currentColor"/>
+      <rect x="1305" y="466" width="353" height="111" transform="rotate(-90 1305 466)" fill="currentColor"/>
+      <rect x="952" y="466" width="353" height="111" transform="rotate(-90 952 466)" fill="currentColor"/>
+      <rect x="111" y="355" width="244" height="111" fill="currentColor"/>
+      <rect y="355" width="244" height="111" transform="rotate(-90 0 355)" fill="currentColor"/>
+    </svg>
+  )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,6 +22,8 @@ import { NAV_ITEMS } from '../constants';
  */
 import { MdMenu } from 'react-icons/md';
 import { BiDownload } from 'react-icons/bi';
+import { Logo } from './Logo';
+
 
 /**
  * Prop
@@ -34,7 +36,9 @@ type prop = {
 const Navbar = ({  setOpenMenu }: prop) => {
   return (
     <div className='bdr-box mx-auto flex h-fit min-h-10 w-full max-w-3xl items-center justify-end gap-2 border-x p-2 tracking-tight sm:min-h-11 md:min-h-12 bg-background/70 backdrop-blur-xs  sm:backdrop-blur-sm'>
+      <div className=" size-fit border bdr-box text-foreground mr-auto"><Logo className="w-20" /></div>
       <div className='hidden w-full justify-end gap-2 sm:flex sm:gap-3 sm:items-center border-r bdr-box px-2'>
+        
         {NAV_ITEMS.map((item) => {
           return (
             <a href={item.href} key={item.label} className='textStyle-heading hover:text-blue-400 hover:duration-100 hover:ease-in-out'>
